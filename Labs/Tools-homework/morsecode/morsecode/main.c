ceaf1cbbffb0637163e692e3f2cf983ff90bfea2
michalsvento <xsvent00@stud.feec.vutbr.cz>
2020-09-26 21:42:40 +0200

    replaced with for

--- Labs/Tools-homework/morsecode/morsecode/main.c
+++ Labs/Tools-homework/morsecode/morsecode/main.c
@@ -1,15 +1,22 @@
 /*
- * morsecode.c
+***********************************************************************
+ * 
+ * Blink a LED and use the function from the delay library.
+ * ATmega328P (Arduino Uno), 16 MHz, AVR 8-bit Toolchain 3.6.2
  *
- * Created: 23. 9. 2020 17:25:36
- * Author : Michal
- */ 
+ * Copyright (c) 2018-2020 Tomas Fryza
+ * Dept. of Radio Electronics, Brno University of Technology, Czechia
+ * This work is licensed under the terms of the MIT license.
+ * 
+ **********************************************************************/
+
+
 
 
 /* Defines -----------------------------------------------------------*/
 #define LED_GREEN   PB5     // AVR pin where green LED is connected
 #define SHORT_DELAY 500      // Delay in miliseconds
-#define LONG_DELAY 1500		// 3 times short delay
+#define LONG_DELAY 1500		// 3 times short delay for dash
 #ifndef F_CPU
 #define F_CPU 16000000      // CPU frequency in Hz required for delay func
 #endif
@@ -17,8 +24,11 @@
 /* Includes ----------------------------------------------------------*/
 #include <util/delay.h>     // Functions for busy-wait delay loops
 #include <avr/io.h>         // AVR device-specific IO definitions
+#include <stdio.h>
 
 /* Variables ---------------------------------------------------------*/
+
+
 
 /* Function prototypes -----------------------------------------------*/
 
@@ -28,6 +38,7 @@
  */
 int main(void)
 {
+	int	a ;   // local variable for more than one dot/dash 
     // Set pin as output in Data Direction Register
     // DDRB = DDRB or 0010 0000
     DDRB = DDRB | (1<<LED_GREEN);  //   << bite shift for PB5   | means or
@@ -38,56 +49,63 @@ int main(void)
 
     // Infinite loop
     while (1)
-    {		// D
+    {																
         // Pause several miliseconds
-        _delay_ms(SHORT_DELAY);
-
+        _delay_ms(SHORT_DELAY);	
+															// D  -..
         // Invert LED in Data Register
         // PORTB = PORTB xor 0010 0000
-        PORTB = PORTB ^ (1<<LED_GREEN); // ^  means xor only when different 
-		
+        PORTB = PORTB ^ (1<<LED_GREEN);						// -
 		_delay_ms(LONG_DELAY);
 		PORTB = PORTB & ~(1<<LED_GREEN);
 		_delay_ms(SHORT_DELAY);
+			
+		for(a=0;a<2;a++)									// ..
+			{	
+				PORTB = PORTB ^ (1<<LED_GREEN);
+				_delay_ms(SHORT_DELAY);
+				PORTB = PORTB & ~(1<<LED_GREEN);
+				_delay_ms(SHORT_DELAY);
+			}
+		
+		_delay_ms(LONG_DELAY);
+		
+	
+															// E  .
+		
 		PORTB = PORTB ^ (1<<LED_GREEN);
 		_delay_ms(SHORT_DELAY);
 		PORTB = PORTB & ~(1<<LED_GREEN);
-		 _delay_ms(SHORT_DELAY);
-		PORTB = PORTB ^ (1<<LED_GREEN);
-		_delay_ms(SHORT_DELAY);
-		PORTB = PORTB & ~(1<<LED_GREEN);
-		_delay_ms(LONG_DELAY);
-		// E
-		
-		PORTB = PORTB ^ (1<<LED_GREEN);
-		_delay_ms(SHORT_DELAY);
-		PORTB = PORTB & ~(1<<LED_GREEN);
 		_delay_ms(LONG_DELAY);
 		
 		
-		// 2
+															// 2  ..---
+		for(a=0;a<2;a++)
+			{
+					PORTB = PORTB ^ (1<<LED_GREEN);			// ..
+					_delay_ms(SHORT_DELAY);
+					PORTB = PORTB & ~(1<<LED_GREEN);
+					_delay_ms(SHORT_DELAY);
+					
+			}
+					
+		for(a=0;a<3;a++)
+				{	
+					PORTB = PORTB ^ (1<<LED_GREEN);			// -
+					_delay_ms(LONG_DELAY);
+					PORTB = PORTB & ~(1<<LED_GREEN);
+					_delay_ms(SHORT_DELAY);
+					
+		}
+	
 		
-		PORTB = PORTB ^ (1<<LED_GREEN);			//. 
 		_delay_ms(SHORT_DELAY);
-		PORTB = PORTB & ~(1<<LED_GREEN);
 		_delay_ms(SHORT_DELAY);
-		PORTB = PORTB ^ (1<<LED_GREEN);			//.
-		_delay_ms(SHORT_DELAY);
-		PORTB = PORTB & ~(1<<LED_GREEN);
-		_delay_ms(SHORT_DELAY);
+		_delay_ms(LONG_DELAY);
 		
-		PORTB = PORTB ^ (1<<LED_GREEN);			// -
-		_delay_ms(LONG_DELAY);
-		PORTB = PORTB & ~(1<<LED_GREEN);
-		_delay_ms(SHORT_DELAY);
-		PORTB = PORTB ^ (1<<LED_GREEN);			// -
-		_delay_ms(LONG_DELAY);
-		PORTB = PORTB & ~(1<<LED_GREEN);
-		_delay_ms(SHORT_DELAY);
-		PORTB = PORTB ^ (1<<LED_GREEN);			// -
-		_delay_ms(LONG_DELAY);
-		PORTB = PORTB & ~(1<<LED_GREEN);
-		_delay_ms(SHORT_DELAY);
+		 // 3 short delays+ 1 long delay + 1 short delay in the beginning = 7 => pause between two words
+		
+		
 		
 		
 		
